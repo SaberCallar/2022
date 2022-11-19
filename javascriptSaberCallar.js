8bdfd3324c79db0b637292df3522e1e2decb00c5
RebeccaJillianBeattie <92300057+RebeccaJillianBeattie@users.noreply.github.com>
2022-11-19 15:56:28 +0100

    Add files via upload

---
+++ javascriptSaberCallar.js
@@ -0,0 +1,154 @@
+//Make coming soon message appear when future editions are clicked
+function comingsoon() {
+	alert("Issue coming soon");
+}
+
+//Make disclaimer statement when click on navbar link "Disclaimer"
+function disclaimer() {
+	alert("The purpose of this website is to explore various types of typographic and layout styles for text documents, as an end-of-course project for the Information Modelling and Web Technologies module of the Masters Degree in Digital Humanities and Digital Knowledge at the University of Bologna, 2021-22, taught by Professor Fabio Vitali.\nThe documents contained in this web site have been selected for their length and complexity. Their publication here is not intended to be an alternative to, or to replace, their original locations, all of which are highlighted in the project's documentation.\nAll copyrights and related rights on the content remain with their original owners. All copyright on the typographic and layout choices are 2022 © Saber Callar");
+}
+
+
+
+//EDITION 1: BORGES
+
+
+//BIBLIOTECA DE BABEL
+
+//Show just the English text of the story
+//If in bilingual mode, hide Spanish text
+function showEnglishBB() {
+	$('#BBEnglish').css('display', 'block')
+	$('#BBSpanish').css('display', 'none')
+}
+
+//Show just the Spanish text of the story
+//If in bilingual mode, hide English text
+function showSpanishBB() {
+	$('#BBSpanish').css('display', 'block')
+	$('#BBEnglish').css('display', 'none')
+}
+
+//Show English and Spanish side-by-side
+function showBilingualBB() {
+	$('#BBSpanish').css('display', 'block')
+	$('#BBEnglish').css('display', 'block')
+} 
+
+
+//PIERRE MENARD
+
+//Show just the English text of the story
+//If in bilingual mode, hide Spanish text
+function showEnglishPM() {
+	$('#PMEnglish').css('display', 'block')
+	$('#PMSpanish').css('display', 'none')
+}
+
+//Show just the Spanish text of the story
+//If in bilingual mode, hide English text
+function showSpanishPM() {
+	$('#PMSpanish').css('display', 'block')
+	$('#PMEnglish').css('display', 'none')
+}
+
+//Show English and Spanish side-by-side
+function showBilingualPM() {
+	$('#PMSpanish').css('display', 'block')
+	$('#PMEnglish').css('display', 'block')
+} 
+
+
+//JARDÍN DE SENDEROS
+
+//Show just the English text of the story
+//If in bilingual mode, hide Spanish text
+function showEnglishJS() {
+	$('#JSEnglish').css('display', 'block')
+	$('#JSSpanish').css('display', 'none')
+}
+
+//Show just the Spanish text of the story
+//If in bilingual mode, hide English text
+function showSpanishJS() {
+	$('#JSSpanish').css('display', 'block')
+	$('#JSEnglish').css('display', 'none')
+}
+
+//Show English and Spanish side-by-side
+function showBilingualJS() {
+	$('#JSSpanish').css('display', 'block')
+	$('#JSEnglish').css('display', 'block')
+} 
+
+
+//EDITION 2: CERVANTES
+
+
+//RINCONETE Y CORTADILLO
+
+//Show just the English text of the story
+//If in bilingual mode, hide Spanish text
+function showEnglishRC() {
+	$('#RCEnglish').css('display', 'block')
+	$('#RCSpanish').css('display', 'none')
+}
+
+//Show just the Spanish text of the story
+//If in bilingual mode, hide English text
+function showSpanishRC() {
+	$('#RCSpanish').css('display', 'block')
+	$('#RCEnglish').css('display', 'none')
+}
+
+//Show English and Spanish side-by-side
+function showBilingualRC() {
+	$('#RCSpanish').css('display', 'block')
+	$('#RCEnglish').css('display', 'block')
+} 
+
+
+//LA FUERZA DE LA SANGRE
+
+//Show just the English text of the story
+//If in bilingual mode, hide Spanish text
+function showEnglishFS() {
+	$('#FSEnglish').css('display', 'block')
+	$('#FSSpanish').css('display', 'none')
+}
+
+//Show just the Spanish text of the story
+//If in bilingual mode, hide English text
+function showSpanishFS() {
+	$('#FSSpanish').css('display', 'block')
+	$('#FSEnglish').css('display', 'none')
+}
+
+//Show English and Spanish side-by-side
+function showBilingualFS() {
+	$('#FSSpanish').css('display', 'block')
+	$('#FSEnglish').css('display', 'block')
+} 
+
+
+//EL LICENCIADO VIDRIERA
+
+//Show just the English text of the story
+//If in bilingual mode, hide Spanish text
+function showEnglishLV() {
+	$('#LVEnglish').css('display', 'block')
+	$('#LVSpanish').css('display', 'none')
+}
+
+//Show just the Spanish text of the story
+//If in bilingual mode, hide English text
+function showSpanishLV() {
+	$('#LVSpanish').css('display', 'block')
+	$('#LVEnglish').css('display', 'none')
+}
+
+//Show English and Spanish side-by-side
+function showBilingualLV() {
+	$('#LVSpanish').css('display', 'block')
+	$('#LVEnglish').css('display', 'block')
+} 
